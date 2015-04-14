6bddac7f2f8b813acfcdd18c0def98ded1a60963
Corey Prophitt <prophitt.corey@gmail.com>
2015-04-14 10:07:39 -0700

    Fixed module naming convention.

--- app/components/home/controllers/home_ctrl_spec.js
+++ app/components/home/controllers/home_ctrl_spec.js
@@ -2,7 +2,7 @@ require('../../../main.js');
 require('../../../../bower_components/angular-mocks/angular-mocks.js');
 
 describe('Home', function() {
-    beforeEach(angular.mock.module('AngularSeedApp'));
+    beforeEach(angular.mock.module('angularSeedApp'));
 
     // Just Pass...
     it('should', function() {expect('true').toEqual('true');});
