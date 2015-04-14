1de097faf1ba8ac7b6aee83415ef1f5023c753f0
Corey Prophitt <prophitt.corey@gmail.com>
2015-04-14 02:43:02 -0700

    Added test.

---
+++ app/components/home/controllers/home_ctrl_spec.js
@@ -0,0 +1,9 @@
+require('../../../main.js');
+require('../../../../assets/libs/angular-mocks/angular-mocks.js');
+
+describe('Home', function() {
+    beforeEach(angular.mock.module('AngularApp'));
+
+    // Just Pass...
+    it('should', function() {expect('true').toEqual('true');});
+});
