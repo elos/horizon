408a950740e02a3d222cc37d1d8deeb0262e0246
Nick Landolfi <nclandolfi@gmail.com>
2015-07-06 17:33:25 -0700

    function() -> function ()

--- app/shared/services/key/key_service.js
+++ app/shared/services/key/key_service.js
@@ -1,12 +1,11 @@
-module.exports = (function() {
-    var KeyService = function() {
+module.exports = (function () {
+    var KeyService = function () {
         this.ENTER = 13;
 
-        this.isEnter = function($event) {
+        this.isEnter = function ($event) {
             return $event.keyCode === this.ENTER;
         };
     };
 
     return KeyService;
-
 }());
