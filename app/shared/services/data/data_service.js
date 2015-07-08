3083ebbe34ff3eb1c90d0b889f7eca99d529c8c8
Nick Landolfi <nclandolfi@gmail.com>
2015-07-07 17:19:25 -0700

    clean up data service

--- app/shared/services/data/data_service.js
+++ app/shared/services/data/data_service.js
@@ -1,55 +1,101 @@
 module.exports = (function() {
     'use strict';
 
-    var DataService = function($http, $log, AccessService, HostService) {
+    var DataService, Types, Transforms, Spaces;
+
+    // --- Types {{{
+    Types = {
+        ID: 'id',
+        Date: 'date',
+        String: 'string',
+        Array: 'array',
+        Map: 'map',
+        Integer: 'integer',
+        Boolean: 'boolean'
+    };
+    // --- }}}
+
+    // --- Transformations {{{
+    function IdentityTransformation(raw) { return raw; }
+    function DateCast(raw) { return new Date(raw); }
+
+    Transforms = {
+        Unmarshal: {},  // from server
+        Marshal: {}   // to server
+    };
+
+    // register unmarshal transforms
+    Transforms.Unmarshal[Types.ID]      = IdentityTransformation;
+    Transforms.Unmarshal[Types.Date]    = DateCast;
+    Transforms.Unmarshal[Types.String]  = IdentityTransformation;
+    Transforms.Unmarshal[Types.Array]   = IdentityTransformation;
+    Transforms.Unmarshal[Types.Map]     = IdentityTransformation;
+    Transforms.Unmarshal[Types.Integer] = IdentityTransformation;
+    Transforms.Unmarshal[Types.Boolean] = IdentityTransformation;
+
+    // register marshal transforms
+    Transforms.Marshal[Types.ID]        = IdentityTransformation;
+    Transforms.Marshal[Types.Date]      = IdentityTransformation;
+    Transforms.Marshal[Types.String]    = IdentityTransformation;
+    Transforms.Marshal[Types.Array]     = IdentityTransformation;
+    Transforms.Marshal[Types.Map]       = IdentityTransformation;
+    Transforms.Marshal[Types.Integer]   = IdentityTransformation;
+    Transforms.Marshal[Types.Boolean]   = IdentityTransformation;
+    // --- }}}
+
+    // --- Spaces {{{
+    Spaces = {
+        'calendar': 'calendars',
+        'credential': 'credentials',
+        'fixture': 'fixtures',
+        'person': 'persons',
+        'schedule': 'schedules',
+        'session': 'sessions',
+        'user': 'users'
+    };
+    // --- }}}
+
+    // --- Kind Helper Object {{{
+    function kind(k, $http, $log, AccessService, HostService) {
+        return {
+            find: function (id) {
+                $log.info('Finding ' + k + ' with id ' + id);
+                var params = {};
+                params[k + "_id"] = id;
+
+                return $http({
+                    method: 'GET',
+                    url: HostService.url('/' + Spaces[k]),
+                    headers: {
+                        'Elos-Auth': AccessService.token()
+                    },
+                    params: params
+                });
+            },
+
+            save: function (model) {
+                $log.info('Saving ' + model + ' as ' + k);
+                return $http({
+                    method: 'POST',
+                    url: HostService.url('/' + Spaces[k]),
+                    headers: {
+                        'Elos-Auth': AccessService.token()
+                    },
+                    data: model
+                });
+            }
+        };
+    }
+    // --- }}}
+
+    DataService = function($http, $log, AccessService, HostService) {
         var service = this;
 
-        service.spaces = {
-            'calendar': 'calendars',
-            'credential': 'credentials',
-            'fixture': 'fixtures',
-            'person': 'persons',
-            'schedule': 'schedules',
-            'session': 'sessions',
-            'user': 'users'
-        };
-
-        service.Types = {
-            ID: 'id',
-            Date: 'date',
-            String: 'string',
-            Array: 'array',
-            Map: 'map',
-            Integer: 'integer',
-            Boolean: 'boolean'
-        };
-
-        service.IdentityTransformation = function (raw) { return raw; };
-
-        service.Transforms = {
-            Marshal: {},
-            Unmarshal: {}
-        };
-
-        service.Transforms.Unmarshal[service.Types.ID] = service.IdentityTransformation;
-        service.Transforms.Unmarshal[service.Types.Date] = function (data) { return new Date(data); };
-        service.Transforms.Unmarshal[service.Types.String] = service.IdentityTransformation;
-        service.Transforms.Unmarshal[service.Types.Array] = service.IdentityTransformation;
-        service.Transforms.Unmarshal[service.Types.Map] = service.IdentityTransformation;
-        service.Transforms.Unmarshal[service.Types.Integer] = service.IdentityTransformation;
-        service.Transforms.Unmarshal[service.Types.Boolean] = service.IdentityTransformation;
-
-        service.Transforms.Marshal[service.Types.ID] = service.IdentityTransformation;
-        service.Transforms.Marshal[service.Types.Date] = service.IdentityTransformation;
-        service.Transforms.Marshal[service.Types.String] = service.IdentityTransformation;
-        service.Transforms.Marshal[service.Types.Array] = service.IdentityTransformation;
-        service.Transforms.Marshal[service.Types.Map] = service.IdentityTransformation;
-        service.Transforms.Marshal[service.Types.Integer] = service.IdentityTransformation;
-        service.Transforms.Marshal[service.Types.Boolean] = service.IdentityTransformation;
+        service.Types = Types;
+        service.Transforms = Transforms;
 
         service.unmarshal = function (model, typemap, data) {
-            /*globals angular*/
-            angular.forEach(Object.keys(typemap), function (field) {
+            Object.keys(typemap).map(function (field) {
                 model[field] = service.Transforms.Unmarshal[typemap[field]](data[field]) || model[field];
             });
 
@@ -59,8 +105,7 @@ module.exports = (function() {
         service.marshal = function (model, typemap) {
             var json = {};
 
-            /*globals angular*/
-            angular.forEach(Object.keys(typemap), function (field) {
+            Object.keys(typemap).map(function (field) {
                 if (model[field] === undefined) { return; }
                 json[field] = service.Transforms.Marshal[typemap[field]](model[field]);
             });
@@ -68,35 +113,8 @@ module.exports = (function() {
             return JSON.stringify(json);
         };
 
-        service.kind = function (kind) {
-            return {
-                find: function (id) {
-                    $log.info('Finding ' + kind + ' with id ' + id);
-                    var params = {};
-                    params[kind + "_id"] = id;
-
-                    return $http({
-                        method: 'GET',
-                        url: HostService.url('/' + service.spaces[kind]),
-                        headers: {
-                            'Elos-Auth': AccessService.token()
-                        },
-                        params: params
-                    });
-                },
-
-                save: function (model) {
-                    $log.info('Saving ' + model + ' as ' + kind);
-                    return $http({
-                        method: 'POST',
-                        url: HostService.url('/' + service.spaces[kind]),
-                        headers: {
-                            'Elos-Auth': AccessService.token()
-                        },
-                        data: model
-                    });
-                }
-            };
+        service.kind = function (k) {
+            return kind(k, $http, $log, AccessService, HostService);
         };
     };
 
